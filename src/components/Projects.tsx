9fd5eaeacf5356ded97a42f76ab02e9bb25592ce
Ishaan-03 <saxenaishaan03@gmail.com>
2024-11-10 22:14:24 +0530

    ..

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -20,7 +20,7 @@ const Projects: React.FC<{ id?: string }> = ({ id }) => {
           <div className="text-3xl font-bold text-slate-300">Projects</div>
           <div className="border-gray-700 border-t-[1px] w-72 mt-5"></div>
         </div>
-        <motion.div  className='flex flex-wrap justify-between'>
+        <motion.div  className='flex flex-wrap mt-8 justify-between'>
           {projects.map((data) => (
             <Card key={data.name} props={data} />
           ))}
